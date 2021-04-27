3002f5631885f7616fd712285d7a5546852d49f7
Saralegui Vallejo, Unai <unai.saralegui@tecnalia.com>
2021-04-27 16:32:05 +0200

    Update create_graph.py

--- create_graph.py
+++ create_graph.py
@@ -4,18 +4,20 @@ import matplotlib.pyplot as plt
 
 import rplanpy
 
-def test_functions(file, out_file='example_graph.png'):
+def test_functions(file, out_file='example_graph.png', plot_original=True):
 
     data = rplanpy.data.RplanData(file)
 
-    fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
-    image = imageio.imread(file)
-    ax[0].imshow(image)
-    ax[0].axis("off")
-    ax[0].set_title("Original image")
-    rplanpy.plot.plot_floorplan(data, ax=ax[1], title="Rooms and doors")
+    ncols = 3 if plot_original else 2
+    fig, ax = plt.subplots(nrows=1, ncols=ncols, figsize=(15, 5))
+    if plot_original:
+        image = imageio.imread(file)
+        ax[0].imshow(image)
+        ax[0].axis("off")
+        ax[0].set_title("Original image")
+    rplanpy.plot.plot_floorplan(data, ax=ax[plot_original+0], title="Rooms and doors")
     ax = rplanpy.plot.plot_floorplan_graph(
-        data=data, with_colors=True, edge_label='door', ax=ax[2],
+        data=data, with_colors=True, edge_label='door', ax=ax[plot_original+1],
         title="Building graph"
     )
     plt.tight_layout()
@@ -25,4 +27,4 @@ def test_functions(file, out_file='example_graph.png'):
 
 if __name__ == '__main__':
     file = 'example.png'
-    test_functions(file, out_file='example_graph.png')
+    test_functions(file, out_file='example_graph.png', plot_original=True)
